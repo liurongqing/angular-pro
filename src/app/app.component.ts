ee457bfa0df27bab8f8680e1e47b311e50caddda
liurongqing <phpgege@163.com>
2018-09-07 18:17:38 +0800

    1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
-
+export interface Tile {
+    color: string;
+    cols: number;
+    rows: number;
+    text: string;
+  }
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'Angular 6.x了啊，更新真快，可以突然放弃啊。';
+export class AppComponent { 
+    tiles: Tile[] = [
+        {text: '头部导航', cols: 10, rows: 1, color: 'lightblue'},
+        {text: '菜单', cols: 2, rows: 4, color: 'lightgreen'},
+        {text: '内容', cols: 8, rows: 5, color: 'lightpink'},
+      ];
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { HeroDetailComponent } from './hero-detail/hero-detail.component';
-import { HeroesComponent } from './heroes/heroes.component';
+import { AppRoutingModule } from './/app-routing.module';
+
+import {
+    MatIconModule,
+    MatListModule,
+    MatGridListModule,
+} from '@angular/material';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { UsersComponent } from './users/users.component';
 
 @NgModule({
     declarations: [
         AppComponent,
-        HeroDetailComponent,
-        HeroesComponent,
+        DashboardComponent,
+        UsersComponent,
     ],
     imports: [
         BrowserModule,
-        FormsModule
+        MatIconModule,
+        MatListModule,
+        MatGridListModule,
+        AppRoutingModule,
+    ],
+    exports: [
+        
     ],
     providers: [],
     bootstrap: [AppComponent]
