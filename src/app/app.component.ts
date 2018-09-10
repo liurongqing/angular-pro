040cde9d6f4ffcc19fd1d95f4ca2a383d72c7bbd
liurongqing <phpgege@163.com>
2018-09-10 18:33:06 +0800

    添加路由

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,37 @@
 import { Component } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+
 export interface Tile {
     color: string;
     cols: number;
     rows: number;
     text: string;
-  }
+}
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
-export class AppComponent { 
+export class AppComponent {
     tiles: Tile[] = [
-        {text: '头部导航', cols: 10, rows: 1, color: 'lightblue'},
-        {text: '菜单', cols: 2, rows: 4, color: 'lightgreen'},
-        {text: '内容', cols: 8, rows: 5, color: 'lightpink'},
-      ];
+        { text: '头部导航', cols: 10, rows: 1, color: 'lightblue' },
+        { text: '菜单', cols: 2, rows: 4, color: 'lightgreen' },
+        { text: '内容', cols: 8, rows: 5, color: 'lightpink' },
+    ];
+
+    currentUrl: String = '/users';
+
+    constructor(private router: Router) {
+        console.log('1...')
+    }
+
+    ngOnInit() {
+        this.router.events
+            .subscribe((event) => {
+                if (event instanceof NavigationEnd) {
+                    this.currentUrl = event.url;
+                }
+            });
+    }
+
 }
